32d408c7df2efc623b56fa0c98bae68a4c5755ae
oscar <oscar.vasquez@zentro.com.mx>
2023-10-23 10:57:13 -0600

    avances

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/tasks' element={<h1>Tasks Page</h1>} />
+        <Route path='/add-task' element={<h1>New Page</h1>} />
+        <Route path='/tasks/:id' element={<h1>Update Page</h1>} />
+        <Route path='/profile' element={<h1>Profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
